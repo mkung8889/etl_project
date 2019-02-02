306c2152fe9ebbfbe43d45f640fca0a1f7dd0d61
Michael <m8464k@gmail.com>
2019-02-02 12:16:42 -0600

    update functions file

---
+++ functions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# to get list of countries for dataset
+def list_countries_un(dataframe):
+    df = dataframe
+    df_drop = df.dropna()
+    country_list = pd.DataFrame(df_drop['Country or Area'].drop_duplicates().reset_index(drop=True))
+    rename_country_list = country_list.rename(columns={
+        'Country or Area': 'country_name'
+    })
+    return rename_country_list
